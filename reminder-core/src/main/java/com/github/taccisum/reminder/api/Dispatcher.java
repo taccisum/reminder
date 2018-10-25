7c74b7d05d450a39921b17c5884c548f2f54ae6e
tac <514162920@qq.com>
2018-10-25 17:32:29 +0800

    rollback

--- reminder-core/src/main/java/com/github/taccisum/reminder/api/Dispatcher.java
+++ reminder-core/src/main/java/com/github/taccisum/reminder/api/Dispatcher.java
@@ -5,9 +5,5 @@ package com.github.taccisum.reminder.api;
  * @since 24/10/2018
  */
 public interface Dispatcher {
-    default int dispatch(String remindCode, Subject subject, ChannelDescriptor channelDescriptor, Object... args) {
-        return dispatch(remindCode, subject, channelDescriptor, null, args);
-    }
-
-    int dispatch(String remindCode, Subject subject, ChannelDescriptor channelDescriptor, TargetSelector targetSelector, Object... args);
+    int dispatch(String remindCode, Subject subject, ChannelDescriptor channelDescriptor, Object... args);
 }
